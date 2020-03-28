487b77e0f241700dc7fc2c30368c89d02fb45831
180896-cse <62785261+180896-cse@users.noreply.github.com>
2020-03-28 12:23:08 +0530

    Add files via upload

---
+++ greatest_no_among_three .cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class addition
+{
+  private:
+    int a,b,c,k;
+  public:
+    void userinput(void);
+    void getoutput(void);
+  
+
+};
+void addition:: userinput(void){
+    cout<<"eneter the value of first no:"<<"\n";
+    cin>>a;
+    cout<<"enter the value of second no:"<<"\n";
+    cin>>b;
+    cout<<"enter the value of  third no:"<<"\n";
+    cin>>c;
+
+}
+void addition:: getoutput(void)
+{
+    k=a>b? a>c?a:c :b>c?b:c;
+    cout<<"the greatest no in between of given no. is:"<<"\n"<<k;
+
+}
+int main()
+{
+  addition shantanu;
+  shantanu.userinput();
+  shantanu.getoutput();
+  
+
+return 0;
+}
